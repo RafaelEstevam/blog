8bcdcdbee2c74613327ce968827a067503b4b78c
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-08 12:43:29 +0000

    Ajust logo

--- src/app/components/header.component.tsx
+++ src/app/components/header.component.tsx
@@ -4,8 +4,8 @@ import SearchComponent from "./search.component";
 const Header = async () => {
 
     return (
-        <header className="w-full rounded-b-2xl flex justify-between items-center gap-32">
-            <div className="flex gap-32">
+        <header className="w-full rounded-b-2xl flex justify-between items-center gap-32 relative">
+            <div className="relative z-10">
                 <nav>
                     <ul className="flex items-center">
                         <li><Link href={'/category/frontend'} className="py-10 px-4 block">Front-end</Link></li>
@@ -15,14 +15,13 @@ const Header = async () => {
                         <li><Link href={'/category/html'} className="py-10 px-4 block">HTML</Link></li>
                     </ul>
                 </nav>
-                <div className="flex flex-col justify-center text-center py-4">
-                    <Link href={'/'}>
-                        <h1 className="font-bold text-4xl">ED&D</h1>
-                    </Link>
-                    {/* <p className="text-xs">Estevam Design <br/> & Desenvolvimento</p> */}
-                </div>
             </div>
-            <div>
+            <div className="flex flex-col justify-center text-center py-4 absolute inset-x-0">
+                <Link href={'/'}>
+                    <h1 className="font-bold text-4xl">ED&D</h1>
+                </Link>
+            </div>
+            <div className="relative z-10">
                 <SearchComponent />
             </div>
         </header>
