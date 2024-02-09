28f4fc384d9eb455673412d2d7863f53c67e647a
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-09 01:59:14 +0000

    Adsense

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,12 +8,13 @@ import Page404 from '../404';
 export async function generateMetadata({ params }: any, parent: ResolvingMetadata): Promise<Metadata> {
     const {slug} = params;
     const variables = { slug };
-    const post:PostProps = await gql_client.request(query, variables);
-    const gallery = post.gallery ? [post?.gallery[0]?.url] : undefined;
+    const {post}:any = await gql_client.request(query, variables);
+    const item:PostProps = post;
+    const gallery = item.gallery ? [item?.gallery[0]?.url] : undefined;
     
     return {
-        title: post.title,
-        description: post.shortText,
+        title: item.title,
+        description: item.shortText,
         openGraph: {
           images: gallery,
         },
