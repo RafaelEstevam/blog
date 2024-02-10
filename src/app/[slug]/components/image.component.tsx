105fcc89fc1f6ae898136764adc54beb140544aa
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-10 02:19:37 +0000

    Fixing build

--- src/app/[slug]/components/image.component.tsx
+++ src/app/[slug]/components/image.component.tsx
@@ -1,6 +1,6 @@
 
 const PostImage = async ({image, title}:any) => {
-    return (
+    return image && image.length > 0 && (
         <div className="w-full bg-slate-800 rounded-2xl min-h-80 max-h-80 overflow-hidden flex flex-row items-center">
             {image.length > 0 && image?.map((img:any) => (
                 <img src={img.url} alt={title} title={title} className="h-fit" key={img.url}/>
--- src/app/[slug]/components/share.component.tsx
+++ src/app/[slug]/components/share.component.tsx
@@ -1,12 +1,12 @@
 import { cookies } from 'next/headers';
 import { PostProps } from "./post.component";
 
-const PostShare = async (post:PostProps) => {
-    const {value}:any = cookies().get('origin');
+const PostShare = async (slug: string) => {
+    // const {value}:any = cookies().get('origin');
     return (
         <div className='flex gap-4 justify-start w-full xl:w-2/3 xl:justify-center text-white'>
             <h4 className='font-bold text-white'>Compartilhar em: </h4>
-            <a className="text-white" href={`https://www.linkedin.com/shareArticle?mini=true&url=${value}/${post.slug}`}>Linkedin</a>
+            <a className="text-white" href={`https://www.linkedin.com/shareArticle?mini=true&url=${slug}`}>Linkedin</a>
         </div>
     )
 };
