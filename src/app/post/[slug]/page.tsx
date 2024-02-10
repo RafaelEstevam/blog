9e69197d00cc821058456431f0629d97c24d6ea0
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-10 01:17:46 +0000

    SSG test

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,6 +2,12 @@ import { PostProps } from "../../[slug]/components/post.component";
 import { queryAllPosts, queryPost } from "../../[slug]/query";
 import { gql_client } from "../../services";
 
+import PostTitle from "../../[slug]/components/title.component";
+import PostImage from "../../[slug]/components/image.component";
+import PostDetails from "../../[slug]/components/details.component";
+import PostContent from "../../[slug]/components/content.component";
+import PostShare from "../../[slug]/components/share.component";
+
 export async function generateStaticParams() {
     const {posts}:any = await gql_client.request(queryAllPosts);
     const postsSlug = posts.map((post:PostProps) => ({slug: post.slug}));
@@ -14,8 +20,21 @@ const Page = async ({params}:any) => {
     const variables = {slug}
     const {post}:any = await gql_client.request(queryPost, variables);
 
+    const title= post.title;
+    const content = post.content;
+    const image = post.gallery;
+    const createdAt = post.createdAt;
+    const autor = post.createdBy.name;
+    const categories = post.categories;
+
     return (
-        <>{post?.title}</>
+        <>
+            <PostImage {...{image, title}} />
+            <PostTitle {...{title, categories}}/>
+            <PostDetails {...{createdAt, autor}} />
+            <PostContent {...{content}}/>
+            <PostShare {...post} />
+        </>
     )
 };
 
