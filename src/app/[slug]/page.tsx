124915ec4a016feba2b79c93873dd1b9431ab9aa
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-10 02:41:49 +0000

    fixing page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,15 +1,20 @@
-import type { Metadata, ResolvingMetadata } from 'next'
+import { PostProps } from "./components/post.component";
+import { queryAllPosts, queryPost } from "./query";
+import { gql_client } from "../services";
 
-import {gql_client} from '../services';
-import {queryPost} from './query';
-import PostComponent, { PostProps } from "./components/post.component";
+import PostTitle from "./components/title.component";
+import PostImage from "./components/image.component";
+import PostDetails from "./components/details.component";
+import PostContent from "./components/content.component";
+import PostShare from "./components/share.component";
+import { Metadata, ResolvingMetadata } from "next";
 
-// export async function generateStaticParams() {
-//     const {posts}:any = await gql_client.request(queryAllPosts);
-//     const postsSlug = posts.map((post:PostProps) => ({slug: post.slug}));
-//     return postsSlug
-// }
- 
+export async function generateStaticParams() {
+    const {posts}:any = await gql_client.request(queryAllPosts);
+    const postsSlug = posts.map((post:PostProps) => ({slug: post.slug}));
+    return postsSlug
+};
+
 export async function generateMetadata({ params }: any, parent: ResolvingMetadata): Promise<Metadata> {
     const {slug} = params;
     const variables = { slug };
@@ -24,13 +29,30 @@ export async function generateMetadata({ params }: any, parent: ResolvingMetadat
           images: gallery,
         },
     }
-}
-
-const Page = async ({params}:any) => {
-    const {slug} = params;
-    return (
-        <PostComponent {...{slug}} />
-    ) 
 };
 
-export default Page
+const Page = async ({params}:any) => {
+
+    const {slug} = params;
+    const variables = {slug}
+    const {post}:any = await gql_client.request(queryPost, variables);
+
+    const title= post.title;
+    const content = post.content;
+    const image = post.gallery;
+    const createdAt = post.createdAt;
+    const autor = post.createdBy.name;
+    const categories = post.categories;
+
+    return (
+        <>
+            <PostImage {...{image, title}} />
+            <PostTitle {...{title, categories}}/>
+            <PostDetails {...{createdAt, autor}} />
+            <PostContent {...{content}}/>
+            <PostShare {...{slug}} />
+        </>
+    )
+};
+
+export default Page;
