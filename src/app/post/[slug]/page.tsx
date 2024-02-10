41722f151b7771719f5efb8ecbed04d9a78e3a75
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-10 02:34:31 +0000

    meta data

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -7,12 +7,29 @@ import PostImage from "../../[slug]/components/image.component";
 import PostDetails from "../../[slug]/components/details.component";
 import PostContent from "../../[slug]/components/content.component";
 import PostShare from "../../[slug]/components/share.component";
+import { Metadata, ResolvingMetadata } from "next";
 
 export async function generateStaticParams() {
     const {posts}:any = await gql_client.request(queryAllPosts);
     const postsSlug = posts.map((post:PostProps) => ({slug: post.slug}));
     return postsSlug
-}
+};
+
+export async function generateMetadata({ params }: any, parent: ResolvingMetadata): Promise<Metadata> {
+    const {slug} = params;
+    const variables = { slug };
+    const {post}:any = await gql_client.request(queryPost, variables);
+    const item:PostProps = post;
+    const gallery = item.gallery ? [item?.gallery[0]?.url] : undefined;
+    
+    return {
+        title: item.title,
+        description: item.shortText,
+        openGraph: {
+          images: gallery,
+        },
+    }
+};
 
 const Page = async ({params}:any) => {
 
