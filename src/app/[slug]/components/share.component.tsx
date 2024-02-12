0e54f2601f861d3834b093745474e506821c9ad0
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-12 18:23:58 +0000

    Fixing linkeding

--- src/app/[slug]/components/share.component.tsx
+++ src/app/[slug]/components/share.component.tsx
@@ -2,7 +2,7 @@ const PostShare = async ({slug}: any) => {
     return (
         <div className='flex gap-4 justify-start w-full xl:w-2/3 xl:justify-center text-white'>
             <h4 className='font-bold text-white'>Compartilhar em: </h4>
-            <a className="text-white" href={`https://www.linkedin.com/shareArticle?mini=true&url=${slug}`}>Linkedin</a>
+            <a className="text-white" href={`https://www.linkedin.com/shareArticle?mini=true&url=${'https://rafaelestevam.netlify.app'}/${slug}`}>Linkedin</a>
         </div>
     )
 };
