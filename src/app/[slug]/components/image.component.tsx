456a916ff6c8a8810eed7e0d95e2dfab205f3bc6
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-13 16:11:23 +0000

    Fixing

--- src/app/[slug]/components/image.component.tsx
+++ src/app/[slug]/components/image.component.tsx
@@ -1,9 +1,9 @@
 
 const PostImage = async ({image, title}:any) => {
     return image && image.length > 0 && (
-        <div className="w-full bg-slate-800 rounded-2xl min-h-80 max-h-80 overflow-hidden flex flex-row items-center">
+        <div className="w-full bg-slate-800 rounded-2xl min-h-80 max-h-80 overflow-hidden flex flex-col items-center justify-center">
             {image.length > 0 && image?.map((img:any) => (
-                <img src={img.url} alt={title} title={title} className="h-fit" key={img.url}/>
+                <img src={img.url} alt={title} title={title} className="h-fit w-full" key={img.url}/>
             ))}
         </div>
     )
