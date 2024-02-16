70b536d0a56aacda708250bf333af11d9b39e342
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-02-16 00:48:57 +0000

    Sobre

---
+++ src/app/me/page.tsx
@@ -0,0 +1,178 @@
+const Page = () => {
+    return (
+        <div className="w-full flex flex-col items-center justify-center gap-10 lg:flex-row lg:items-start">
+            <div className="w-full lg:w-2/3 flex flex-col gap-24">
+                <div>
+                    <h1 className="text-6xl text-center">Rafael Estevam de Oliveira</h1>
+                    <p className="text-2xl text-center">Engenheiro de software</p>
+                </div>
+
+                <div className="gap-10 rounded-2xl bg-slate-700 p-10"></div>
+
+                <div className="flex w-full gap-4">
+                    <div className="md:w-1/2 flex flex-col gap-10 items-end">
+                        <h2 className="text-5xl">Graduação</h2>
+                        <div className="flex flex-col gap-10">
+                            <div className="flex gap-10 flex-row-reverse">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div className="flex flex-col items-end">
+                                    <h3>Banco de dados</h3>
+                                    <p>2023 - </p>
+                                    <p>FATEC - Faculdade de tecnologia de SP</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-10 flex-row-reverse">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div className="flex flex-col items-end">
+                                    <h3>Banco de dados</h3>
+                                    <p>2023 - </p>
+                                    <p>FATEC - Faculdade de tecnologia de SP</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className="text-5xl">&</h2>
+                    </div>
+                    <div className="md:w-1/2 flex flex-col gap-10">
+                        <h2 className="text-5xl">Experiência</h2>
+                        <div className="flex flex-col gap-10">
+                            <div className="flex gap-10">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div>
+                                    <h3>Printi</h3>
+                                    <p>2023 - </p>
+                                    <p>Engenheiro front-end Pleno</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-10">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div>
+                                    <h3>Printi</h3>
+                                    <p>2023 - </p>
+                                    <p>Engenheiro front-end Pleno</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-10">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div>
+                                    <h3>Printi</h3>
+                                    <p>2023 - </p>
+                                    <p>Engenheiro front-end Pleno</p>
+                                </div>
+                            </div>
+                            <div className="flex gap-10">
+                                <div className="w-10 h-10 bg-slate-700"></div>
+                                <div>
+                                    <h3>Printi</h3>
+                                    <p>2023 - </p>
+                                    <p>Engenheiro front-end Pleno</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="text-5xl text-center">Conhecimentos</h2>
+                </div>
+                
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div className="grid gap-4">
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                        <div className="bg-slate-700 rounded-2xl p-4">
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                            <p>asdfasd</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+export default Page;
