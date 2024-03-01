788ae5dc1d0dc91c56ef87572a93b1a66b35759a
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-03-01 19:47:53 +0000

    ESComponents

---
+++ src/app/escomponents/page.tsx
@@ -0,0 +1,18 @@
+import ESComponents from '@rafaelestevam/escomponents/src'
+
+const ESComponentsPage = () => {
+    return (
+        <>
+            <ESComponents.Button
+                id='primary'
+                color='primary'
+                label='Button'
+                size='sm'
+                type='button'
+                variant='filled'
+            />
+        </>
+    )
+}
+
+export default ESComponentsPage;
