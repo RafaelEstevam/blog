8e4b393bf238fa798d3b9bd67271ffbe2d1d13df
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-03-05 22:32:04 +0000

    Improvments to main image

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,41 @@
 const env = process.env.NODE_ENV || 'development';
 
 const nextConfig = {
-    development: {
-        api: '',
-        NEXT_PUBLIC_GA: ''
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'media.graphassets.com',
+            port: '',
+            pathname: '/**',
+        },
+        ],
     },
-    staging: {
-        api: '',
-        NEXT_PUBLIC_GA: 'G-N24CHGYB0M'
-    },
-    production: {
-        api: '',
-        NEXT_PUBLIC_GA: 'G-N24CHGYB0M'
-    }
-}[env];
+}
+
+// const nextConfig = {
+//     development: {
+//         api: '',
+//         NEXT_PUBLIC_GA: ''
+//     },
+//     staging: {
+//         api: '',
+//         NEXT_PUBLIC_GA: 'G-N24CHGYB0M'
+//     },
+//     production: {
+//         api: '',
+//         NEXT_PUBLIC_GA: 'G-N24CHGYB0M'
+//     },
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: 'https',
+//             hostname: 'media.graphassets.com',
+//             port: '',
+//             pathname: '/**',
+//           },
+//         ],
+//     }
+// }[env];
 
 export default nextConfig;
--- src/app/[slug]/components/image.component.tsx
+++ src/app/[slug]/components/image.component.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 const PostImage = async ({image, title}:any) => {
     return image && image.length > 0 && (
         <div className="w-full bg-slate-800 rounded-2xl min-h-80 max-h-80 overflow-hidden flex flex-col items-center justify-center">
             {image.length > 0 && image?.map((img:any) => (
-                <img src={img.url} alt={title} title={title} className="h-fit w-full" key={img.url}/>
+                <Image height={200} width={400} src={img.url} alt={title} title={title} className="h-fit w-full" key={img.url}/>
             ))}
         </div>
     )
